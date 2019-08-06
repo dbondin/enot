f31182573df42cfd55dbced997dd3cb1a92ba8de
Dmitry Bondin <dbondin@ya.ru>
2019-08-07 00:18:25 +0300

    further fixes

---
+++ src/main/java/ru/applmath/enot/controllers/DelmeController.java
@@ -0,0 +1,20 @@
+package ru.applmath.enot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.applmath.enot.dto.DelmeDto;
+
+@RestController
+public class DelmeController {
+
+	@RequestMapping(method=RequestMethod.GET, path="/delme")
+	public DelmeDto delme() {
+		final DelmeDto result = new DelmeDto();
+		result.setStr("Hello");
+		result.setI(23);
+		result.setD(Math.PI);
+		return result;
+	}
+}
---
+++ src/main/java/ru/applmath/enot/dto/DelmeDto.java
@@ -0,0 +1,32 @@
+package ru.applmath.enot.dto;
+
+public class DelmeDto {
+	
+	private int i;
+	private double d;
+	private String str;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public double getD() {
+		return d;
+	}
+
+	public void setD(double d) {
+		this.d = d;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+}
